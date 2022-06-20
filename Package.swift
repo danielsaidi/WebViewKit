026a186ef50cf8ce1c6c2d54954c4d9f3f5f27c8
Daniel Saidi <daniel.saidi@gmail.com>
2022-06-20 15:28:39 +0200

    Cleanup package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,7 +9,6 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WebViewKit",
             targets: ["WebViewKit"]),
@@ -19,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WebViewKit",
             dependencies: []),
